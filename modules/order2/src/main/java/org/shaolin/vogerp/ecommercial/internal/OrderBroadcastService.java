51e65d7d4b51b119d9280d94463af087a4236bc1
shaolinwu <shao.lin@live.com>
2018-03-11 23:43:06 +0800

    testing

--- modules/order2/src/main/java/org/shaolin/vogerp/ecommercial/internal/OrderBroadcastService.java
+++ modules/order2/src/main/java/org/shaolin/vogerp/ecommercial/internal/OrderBroadcastService.java
@@ -2,32 +2,42 @@ package org.shaolin.vogerp.ecommercial.internal;
 
 import java.util.List;
 
-import org.shaolin.bmdp.runtime.AppContext;
 import org.shaolin.bmdp.workflow.be.INotification;
-import org.shaolin.bmdp.workflow.be.NotificationImpl;
-import org.shaolin.bmdp.workflow.coordinator.ICoordinatorService;
-import org.shaolin.vogerp.commonmodel.be.IPersonalAccount;
-import org.shaolin.vogerp.commonmodel.be.PersonalAccountImpl;
+import org.shaolin.vogerp.commonmodel.be.IOrganization;
+import org.shaolin.vogerp.commonmodel.be.OrganizationImpl;
+import org.shaolin.vogerp.commonmodel.ce.OrgType;
 import org.shaolin.vogerp.commonmodel.dao.CommonModel;
 
 public class OrderBroadcastService {
 
+	/**
+	 * TODO: R tree algorithem.
+	 * 
+	 * @param message
+	 */
 	public void broadcast(INotification message) {
 		if (message.getNeedBroadcast()) {
-			message.getCity();
-			message.getPartyType();
 			
-			PersonalAccountImpl account = new PersonalAccountImpl();
-			account.setLocationInfo(message.getCity());
-			List<IPersonalAccount> result = CommonModel.INSTANCE.searchUserAccount(account, null, 0, 1);
-			for (IPersonalAccount acc: result) {
-				NotificationImpl msg = new NotificationImpl();
-				msg.setPartyId(acc.getPersonalId());
-				msg.setDescription(message.getDescription());
+			OrganizationImpl scFlow = new OrganizationImpl();
+			scFlow.setOrgType(OrgType.COMPANY);
+			scFlow.setType("");
+			List<IOrganization> result = CommonModel.INSTANCE.searchOrganization(scFlow, null, 0, 1);
+			if (result != null && result.size() > 0) {
 				
-				ICoordinatorService service = (ICoordinatorService)AppContext.get().getService(ICoordinatorService.class);
-                service.addNotification(message, true);
 			}
+			
+			
+//			PersonalAccountImpl account = new PersonalAccountImpl();
+//			account.setLocationInfo(message.getCity());
+//			List<IPersonalAccount> result = CommonModel.INSTANCE.searchUserAccount(account, null, 0, -1);
+//			for (IPersonalAccount acc: result) {
+//				NotificationImpl msg = new NotificationImpl();
+//				msg.setPartyId(acc.getPersonalId());
+//				msg.setDescription(message.getDescription());
+//				
+//				ICoordinatorService service = (ICoordinatorService)AppContext.get().getService(ICoordinatorService.class);
+//                service.addNotification(message, true);
+//			}
 		}
 	}
 
